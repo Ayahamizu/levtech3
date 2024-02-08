33fc210941265909fdeea1f1f920c7165956ce31
Ayahamizu <ayacoco.1127512@gmail.com>
2024-02-08 16:31:11 +0900

    投稿記事作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,16 +9,19 @@ class PostController extends Controller
 {
 
    public function index(Post $post)
-{
-    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
-} 
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    } 
    public function show(Post $post)
-{
-    return view('posts.show')->with(['post' => $post]);   
- //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    {
+        return view('posts.show')->with(['post' => $post]);   
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    public function create()
+    {
+        return view('posts/create');
+    }
 }
-}
-
 
 
 ?>
