e577b1d667cb47d1c1475b87e500a691206a6c64
Ayahamizu <ayacoco.1127512@gmail.com>
2024-02-18 19:21:23 +0900

    9-3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
+<x-app-layout>
         <h1>Blog Name</h1>
         <a href="/posts/create">create</a>
         <div class='posts'>
@@ -26,6 +18,7 @@
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
+        {{Auth::user()->email}}
          <script>
              function deletePost(id) {
                  'use strict'
@@ -35,5 +28,4 @@
                  }
              }
          </script>
-    </body>
-</html>
+    </x-app-layout>
